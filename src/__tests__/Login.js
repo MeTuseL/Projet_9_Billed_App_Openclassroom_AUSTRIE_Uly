fe56c8138d4086e3986751d2143d1beb25cc7b8e
MeTuseL <austrie-uly@hotmail.fr>
2023-08-05 17:33:25 -0400

    change directory  of frontEnd App

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -98,7 +98,7 @@ describe("Given that I am a user on login page", () => {
       login.login = jest.fn().mockResolvedValue({});
       form.addEventListener("submit", handleSubmit);
       fireEvent.submit(form);
-      expect(handleSubmit).toHaveBeenCalled();
+      expect(handleSubmit).toHaveBeenCalled();//
       expect(window.localStorage.setItem).toHaveBeenCalled();
       expect(window.localStorage.setItem).toHaveBeenCalledWith(
         "user",
@@ -112,7 +112,7 @@ describe("Given that I am a user on login page", () => {
     });
 
     test("It should renders Bills page", () => {
-      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();
+      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();//
     });
   });
 });
